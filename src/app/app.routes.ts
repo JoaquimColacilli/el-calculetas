d901e09a67db11a25cbef22dbecd8b6bfa5d3022
Joaquim <joaquimcolacilli9@gmail.com>
2024-09-03 20:27:49 -0300

    se commitea

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+];
