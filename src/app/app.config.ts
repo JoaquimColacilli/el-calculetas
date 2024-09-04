90b7fe587638509a1542fb42b95b902382e164ac
Joaquim <joaquimcolacilli9@gmail.com>
2024-09-04 14:27:39 -0300

    se pasa string conexion bdd a archivo environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,23 +6,14 @@ import { provideAuth, getAuth } from '@angular/fire/auth'; // Importa provideAut
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { AuthService } from './services/auth.service';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyArexJij981sVCmu8dcNKfQ1kBdlob5mIs',
-  authDomain: 'el-calculetas.firebaseapp.com',
-  projectId: 'el-calculetas',
-  storageBucket: 'el-calculetas.appspot.com',
-  messagingSenderId: '48218338812',
-  appId: '1:48218338812:web:b166ec27532f14fb2a2e79',
-  measurementId: 'G-J6642M87D8',
-};
+import { environment } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     importProvidersFrom([
-      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideFirebaseApp(() => initializeApp(environment)),
       provideFirestore(() => getFirestore()),
       provideAuth(() => getAuth()),
     ]),
