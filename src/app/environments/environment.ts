a66ef2494c6d3e07bd27424279da1578ae7665b2
Joaquim <joaquimcolacilli9@gmail.com>
2024-09-04 15:27:11 -0300

    Removed environment.ts from version control

--- src/app/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  apiKey: 'AIzaSyArexJij981sVCmu8dcNKfQ1kBdlob5mIs',
-  authDomain: 'el-calculetas.firebaseapp.com',
-  projectId: 'el-calculetas',
-  storageBucket: 'el-calculetas.appspot.com',
-  messagingSenderId: '48218338812',
-  appId: '1:48218338812:web:b166ec27532f14fb2a2e79',
-  measurementId: 'G-J6642M87D8',
-};
